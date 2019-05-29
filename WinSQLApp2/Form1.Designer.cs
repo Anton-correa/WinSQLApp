70a900c1c7fecdeb7702a21dbe05b731c0a9b2a8
Antonio J. Correa Lopez <ajcorrea@uprcm.local>
2019-05-29 09:54:52 -0400

    Add project files.

---
+++ WinSQLApp2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WinSQLApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqlConn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.getTablbtn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // sqlConn
+            // 
+            this.sqlConn.Location = new System.Drawing.Point(22, 24);
+            this.sqlConn.Name = "sqlConn";
+            this.sqlConn.Size = new System.Drawing.Size(167, 23);
+            this.sqlConn.TabIndex = 0;
+            this.sqlConn.Text = "Verify SQL Connection";
+            this.sqlConn.UseVisualStyleBackColor = true;
+            this.sqlConn.Click += new System.EventHandler(this.sqlConn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(219, 85);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(569, 193);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // getTablbtn
+            // 
+            this.getTablbtn.Location = new System.Drawing.Point(22, 85);
+            this.getTablbtn.Name = "getTablbtn";
+            this.getTablbtn.Size = new System.Drawing.Size(167, 23);
+            this.getTablbtn.TabIndex = 2;
+            this.getTablbtn.Text = "Pass Value Table";
+            this.getTablbtn.UseVisualStyleBackColor = true;
+            this.getTablbtn.Click += new System.EventHandler(this.getTablbtn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 148);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(167, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(9, 279);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 4;
+            this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(265, 307);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(487, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.getTablbtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.sqlConn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sqlConn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button getTablbtn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WinSQLApp2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WinSQLApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string connstring = string.Format("Server=localhost; database=testdb; UID=root; password=");
+            connection = new MySqlConnection(connstring);
+            connection.Open();
+
+            string query = "SELECT Nombre FROM users";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string name = reader.GetString(0); //first column
+                comboBox1.Items.Add(name);
+            }
+        }
+
+
+        private MySqlConnection connection = null;
+        private void sqlConn_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("Connection open");
+          
+            
+        }
+
+        private void getTablbtn_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM users";
+            var cmd = new MySqlCommand(query, connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string name = reader.GetString(0); //first column
+                string dept = reader.GetString(1); //second column
+                Console.WriteLine("-> "+ name + ", " + dept);
+                richTextBox1.AppendText("-> " + name + ", " + dept + "\n");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            connection.Close();
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            label1.Text = monthCalendar1.SelectionRange.Start.ToString("dd MMM yyyy");
+        }
+    }
+}
